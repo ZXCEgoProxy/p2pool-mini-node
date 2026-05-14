d8ffeb772c3389c2ec1ae6cbf465d289f39bebe9
LUX1984 <kalilux1984@gmail.com>
2026-05-14 14:42:18 +0300

    Add --detach and --non-interactive to monerod to keep it running

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ WALLET=${WALLET_ADDRESS:-"4AL6QjWtF4RCyPzPT7Ew3khPuqhmcJC9BQe9Cpxvv3noevJyp23YLT
 
 # Start Monero daemon
 echo "Starting Monero daemon..."
-/monero/monerod --zmq-pub tcp://127.0.0.1:18083 --out-peers 32 --in-peers 64 --add-priority-node=p2pmd.xmrvsbeast.com:18080 --add-priority-node=nodes.hashvault.pro:18080 --enforce-dns-checkpointing --enable-dns-blocklist </dev/null &
+/monero/monerod --zmq-pub tcp://127.0.0.1:18083 --out-peers 32 --in-peers 64 --add-priority-node=p2pmd.xmrvsbeast.com:18080 --add-priority-node=nodes.hashvault.pro:18080 --enforce-dns-checkpointing --enable-dns-blocklist --detach --non-interactive --log-file /dev/stdout
 
 # Wait a bit for monerod to start
 sleep 30
